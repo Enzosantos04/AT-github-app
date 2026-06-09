0804e95e5b31d58e7166626d0b7fd19c908ae8a1
Enzosantos04 <enzohfs@gmail.com>
2026-06-09 15:05:52 -0300

    feat: add authContext for sign in and sign out using githube token

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+//TAREFA 2
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [userToken, setUserToken] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signIn = async (token) => {
+    setUserToken(token);
+    await AsyncStorage.setItem("userToken", token);
+  };
+
+  const signOut = async () => {
+    setUserToken(null);
+    await AsyncStorage.removeItem("userToken");
+  };
+
+  useEffect(() => {
+    const loadStorageData = async () => {
+      const savedToken = await AsyncStorage.getItem("userToken");
+      if (savedToken) {
+        setUserToken(savedToken);
+      }
+    };
+    loadStorageData();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ userToken, isLoading, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
