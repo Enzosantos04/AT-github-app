0d3a5a82610f8d01bb38555cc34669e2663a9ae1
Enzosantos04 <enzohfs@gmail.com>
2026-06-10 18:11:54 -0300

    fix: add missing import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./navigation/RootNavigator";
 import { StatusBar } from "expo-status-bar";
 import AuthProvider from "./context/AuthContext";
+import GithubProvider from "./context/githubContext";
 
 //TAREFA 1
 const App = () => {
