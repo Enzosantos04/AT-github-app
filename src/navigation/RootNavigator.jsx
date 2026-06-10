8799013843c103ec1e7f3066f82042f7cca2be0b
Enzosantos04 <enzohfs@gmail.com>
2026-06-10 19:20:21 -0300

    feat: add DrawerNavigator

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
-import TabNavigator from "./TabNavigator";
+import DrawerNavigator from "./DrawerNavigator";
 import { useAuth } from "../context/AuthContext";
 
 const Stack = createStackNavigator();
 
 //TAREFA 1
 const RootNavigator = () => {
-  const { userToken, isLoading } = useAuth();
+  const { userToken } = useAuth();
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
       initialRouteName="GitHub App"
     >
       {userToken ? (
-        <Stack.Screen name="Main" component={TabNavigator} />
+        <Stack.Screen name="Main" component={DrawerNavigator} />
       ) : (
         <>
           <Stack.Screen name="GitHub App" component={LoginScreen} />
