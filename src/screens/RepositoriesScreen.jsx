2b5ca0712cb2a3f8110f5c3098b4da9057369a3a
Enzosantos04 <enzohfs@gmail.com>
2026-06-11 16:03:52 -0300

    feat: add ProgressBar component on RepositoriesScreen

--- src/screens/RepositoriesScreen.jsx
+++ src/screens/RepositoriesScreen.jsx
@@ -3,15 +3,20 @@ import { View, Text } from "react-native";
 import { styles } from "../styles/Main.styles";
 import { useGithubContext } from "../context/githubContext";
 import RepositoriesList from "../components/RepositoriesList";
+import ProgressBar from "../components/ProgressBar";
 
 //TAREFA 7
 const RepositoriesScreen = () => {
-  const { repos, isLoading } = useGithubContext();
+  const { repos, isLoading, useData } = useGithubContext();
 
   return (
     <View style={styles.container}>
       <View style={styles.headerSection}>
         <Text style={styles.title}>Repositories</Text>
+        <ProgressBar
+          current={repos.length}
+          total={useData?.public_repos || 0}
+        />
         <Text style={styles.subtitle}>Manage your GitHub projects</Text>
       </View>
 
