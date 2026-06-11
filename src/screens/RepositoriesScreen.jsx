a101d8213b8ca10c2a16e6f8ce434dec7fb81850
Enzosantos04 <enzohfs@gmail.com>
2026-06-11 11:37:18 -0300

    refactor: add RepositoriesList component

--- src/screens/RepositoriesScreen.jsx
+++ src/screens/RepositoriesScreen.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Octicons } from "@expo/vector-icons";
 import { styles } from "../styles/Main.styles";
-import { Colors } from "../styles/theme";
-//TAREFA 1
+import { useGithubContext } from "../context/githubContext";
+import RepositoriesList from "../components/RepositoriesList";
+
+//TAREFA 7
 const RepositoriesScreen = () => {
+  const { repos, isLoading } = useGithubContext();
+
   return (
     <View style={styles.container}>
       <View style={styles.headerSection}>
@@ -12,19 +15,7 @@ const RepositoriesScreen = () => {
         <Text style={styles.subtitle}>Manage your GitHub projects</Text>
       </View>
 
-      <View style={styles.card}>
-        <Octicons
-          name="repo"
-          size={48}
-          color={Colors.inkMuted}
-          style={styles.emptyStateIcon}
-        />
-        <Text style={styles.cardTitle}>No repositories yet</Text>
-        <Text style={styles.cardText}>
-          Once you start tracking repositories, they will appear here in a
-          structured list.
-        </Text>
-      </View>
+      <RepositoriesList repos={repos} isLoading={isLoading} />
     </View>
   );
 };
