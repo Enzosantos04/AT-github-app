ba1b9defdc5408fbca634064cdda05cbe9f20ca9
Enzosantos04 <enzohfs@gmail.com>
2026-06-17 10:54:31 -0300

    feat: add register user simulation

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -11,9 +11,25 @@ import { styles } from "../styles/LoginScreen.styles";
 import { Colors } from "../styles/theme";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { useAuth } from "../context/AuthContext";
+import { Alert } from "react-native";
+import { useState } from "react";
 
 //TAREFA 1
 const RegisterScreen = ({ navigation }) => {
+  const { register } = useAuth();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = (username, password) => {
+    if (!username || !password) {
+      Alert.alert("Erro", "Nome de usuário e senha são obrigatórios.");
+      return;
+    }
+    register(username, password);
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -30,15 +46,25 @@ const RegisterScreen = ({ navigation }) => {
         </View>
 
         <View style={styles.card}>
-          <Input label="Username" placeholder="Enter a username" />
+          <Input
+            label="Username"
+            placeholder="Enter a username"
+            value={username}
+            onChangeText={setUsername}
+          />
           <Input label="Email address" placeholder="Enter your email" />
           <Input
             label="Password"
             placeholder="Create a password"
             secureTextEntry
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <Button title="Create account" />
+          <Button
+            title="Create account"
+            onPress={() => handleRegister(username, password)}
+          />
         </View>
 
         <View style={styles.footer}>
